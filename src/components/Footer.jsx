d186842345c1223ba5dd9c95c72cd68910e4dc00
Pranjal Kumar <pranjalkumardev@gmail.com>
2023-06-12 16:52:22 +0530

    Updated links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       outline:'1px solid darkslategray'
     }}>
       <Typography variant="h6" py={1} sx={{ color: "#dedede", textAlign:'center' }}>
-           &copy; {new Date().getFullYear()} | <a href="https://github.com/pranjal-barnwal/devTube" target="_blank" rel="noopener noreferrer" style={{fontWeight:'bold'}}><span style={{color:'#fff'}}>Dev</span><span style={{color:'#fc1503'}}>Tube</span></a> | <a href="https://www.linkedin.com/in/pranjal-kumar-ac/" target="_blank" rel="noopener noreferrer" style={{color:'#dedede'}}>Pranjal Kumar</a>
+           &copy; {new Date().getFullYear()} | <a href="https://github.com/pranjal-barnwal/devTube" target="_blank" rel="noopener noreferrer" style={{fontWeight:'bold'}}><span style={{color:'#fff'}}>Dev</span><span style={{color:'#fc1503'}}>Tube</span></a> | <a href="https://www.linkedin.com/in/pranjal-barnwal" target="_blank" rel="noopener noreferrer" style={{color:'#dedede'}}>Pranjal Kumar</a>
         </Typography>
     </Stack>
   )
